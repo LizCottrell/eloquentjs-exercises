4c880a5677a6d70c2534cd91f8b12db9cab7812b
Elizabeth Cottrell <elizabeth@openforge.io>
2018-09-25 09:11:16 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Eloquent JS Exercises
+
+// Chapter 2
+
+// 2-1. Looping a triangle
+
+for (let number = 0; number <= 7; number = number + 1) {
+
+    hash = // display the number's value in #'s so 2 = ##
+
+    console.log(hash);
+}
